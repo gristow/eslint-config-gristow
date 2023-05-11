1cdb85c287cf21827c3a19ba6dbcac2ac23a3548
Greg Ristow <gristow@utheory.com>
2023-05-10 23:16:01 -0400

    feat(svelte): Separate into a svelte.eslintrc.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,43 +19,6 @@ module.exports = {
   extends: ['plugin:svelte/recommended'],
   overrides: [
     {
-      files: ['*.svelte'],
-      plugins: ['@typescript-eslint', 'eslint-plugin-tsdoc'],
-      extends: ['airbnb-typescript/base'],
-      rules: {
-        ...rules,
-        /**
-         * eslint-plugin-svelte3 has issues, basically, with any eslint fix that creates a
-         * multi-line fix. So, lots has to be disabled... even so, I'm sure I've missed some
-         * see https://github.com/sveltejs/eslint-plugin-svelte3/blob/master/OTHER_PLUGINS.md
-         */
-        'import/first': 'off',
-        'import/no-cycle': 'off',
-        'import/no-duplicates': 'off',
-        'import/no-mutable-exports': 'off',
-        'import/no-unresolved': 'off',
-        'import/no-useless-path-segments': 'off',
-        // Prettier will add an empty line before end of </script> so this has to be off.
-        'no-multiple-empty-lines': 'off',
-        // Prettier will sometimes add a new line after =, so disable this:
-        'operator-linebreak': 'off',
-        '@typescript-eslint/indent': 'off',
-        '@typescript-eslint/no-var-requires': 'warn',
-        '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
-        'no-useless-constructor': 'off',
-        // no-dupe-class-members should be removed when this PR lands:
-        // https://github.com/typescript-eslint/typescript-eslint/pull/1492
-        'no-dupe-class-members': 'off',
-        // So we can allow overloading, and list of class props defined
-        'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
-        '@typescript-eslint/no-useless-constructor': 'error',
-        'tsdoc/syntax': 'warn',
-        // We get these via TS checking, and w/ import type we might have 2 entries,
-        // one for named, one for default, one for actual import.
-        'no-duplicate-imports': 'off',
-      },
-    },
-    {
       files: ['*.js'],
       plugins: ['prettier', 'jsdoc'],
       extends: ['airbnb/base', 'prettier'],
@@ -115,7 +78,6 @@ module.exports = {
         jsdoc: {
           mode: 'typescript',
         },
-        'svelte/ignore-styles': () => true,
       },
     },
   ],
