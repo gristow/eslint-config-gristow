701ccc8055a12a08a69288bb9a743ef09d74cc31
Greg Ristow <gristow@utheory.com>
2023-05-12 13:49:42 -0400

    fix: import rule should allow aliases not listed in dependencies

--- rules/svelte-rules.cjs
+++ rules/svelte-rules.cjs
@@ -1,7 +1,6 @@
 module.exports = {
   // Must disable to make 2-way data binding possible
   'import/no-mutable-exports': 'off',
-  'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   'no-labels': 'off',
   'no-restricted-syntax': 'off',
   // In svelte we often have to init an export ot undefined to mark it as an optional
